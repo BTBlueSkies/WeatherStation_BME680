ec56a6f2d42ed93e398bcb1d44f066230b167efc
K Easterly <msdn@bigtexansoftware.com>
2019-08-13 13:23:03 -0400

    initial demo code running

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,80 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED13 13
+
+#define BME_SCK 15
+#define BME_MOSI 16
+#define BME_MISO 14
+#define BME_CS 11
+Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
+
+#define SEALEVELPRESSURE_HPA 1011.2
+
+void setup()
+{  
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  Serial.println(F("BME680 async test"));
+
+  if (!bme.begin())
+  {
+    Serial.println(F("Could not find a valid BME600 sensor, check wiring!"));
+    while (1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Tell BME680 to begin measurement.
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading :("));
+    return;
+  }
+  Serial.print(F("Reading started at "));
+  Serial.print(millis());
+  Serial.print(F(" and will finish at "));
+  Serial.println(endTime);
+
+  Serial.println(F("You can do other work during BME680 measurement."));
+  delay(50); // This represents parallel work.
+  // There's no need to delay() until millis() >= endTime: bme.endReading()
+  // takes care of that. It's okay for parallel work to take longer than
+  // BME680's measurement time.
+
+  // Obtain measurement results from BME680. Note that this operation isn't
+  // instantaneous even if milli() >= endTime due to I2C/SPI latency.
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading :("));
+    return;
+  }
+  Serial.print(F("Reading completed at "));
+  Serial.println(millis());
+
+  Serial.print(F("Temperature = "));
+  Serial.print(bme.temperature);
+  Serial.println(F(" *C"));
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(F(" hPa"));
+
+  Serial.print(F("Humidity = "));
+  Serial.print(bme.humidity);
+  Serial.println(F(" %"));
+
+  Serial.print(F("Gas = "));
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(F(" KOhms"));
+
+  Serial.print(F("Approx. Altitude = "));
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(F(" m"));
+
+  Serial.println();
+  delay(2000);
 }
